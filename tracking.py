f6de9b617e634404d2e4cbfd2600ce77f33d95f3
Guilherme Campello <gui.campello98@gmail.com>
2020-12-11 02:11:03 -0300

    Add calibration data and analysis

--- tracking.py
+++ tracking.py
@@ -3,11 +3,11 @@ from flask import request
 import json
 
 app = Flask(__name__)
-file = open("calibrations.txt","a+")
 
 @app.route('/origin', methods=['POST'])
 def calibrations_collection():
-  global file
   if request.method == 'POST':
-    file.write(request.data.decode('ascii') + "\n")
+    with open("calibrations.csv","a+") as file:
+      file.write(request.data.decode('ascii') + "\n")
+      file.close()
     return "Ok", 200
